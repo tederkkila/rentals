36fac24410c74430359043bae12089f438b2fd33
Ted <tederkkila@gmail.com>
2026-04-18 11:32:06 +1200

    fix footer width and spacing

--- modules/tenants/ui/components/footer.tsx
+++ modules/tenants/ui/components/footer.tsx
@@ -10,8 +10,11 @@ const poppins = Poppins({
 
 export const Footer = () => {
     return (
-        <footer className="border-t font-medium bg-white">
-            <div className="max-w-(--breakpoint-xl) mx-auto flex items-center h-full gap-2 px-4 py-6 lg:px-12">
+        <footer className="border-t font-medium bg-white
+            max-w-full sm:max-w-[calc(100vw-2rem)] lg:max-w-7xl
+            sm:border-x
+            mx-auto">
+            <div className="max-w-(--breakpoint-xl) mx-auto flex items-center h-full gap-2 px-4 py-6">
                 <p>Powered by</p>
                 <Link href={process.env.NEXT_PUBLIC_APP_URL!}>
                   <span className={cn("text-2xl font-semibold", poppins.className)}>
