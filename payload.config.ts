5991dd9ab24f4d2f73ce00145e160488639e4546
Ted <tederkkila@gmail.com>
2026-05-23 15:41:01 +1200

    add vercel blob storage token BLOB_READ_WRITE_TOKEN

--- payload.config.ts
+++ payload.config.ts
@@ -103,7 +103,7 @@ export default buildConfig({
             collections: {
                 media: true,
             },
-            token: process.env.VERCEL_BLOB_STORAGE_TOKEN || '',
+            token: process.env.BLOB_READ_WRITE_TOKEN || '',
         })
     ],
     email: resendAdapter({
